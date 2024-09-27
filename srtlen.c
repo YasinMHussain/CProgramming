fe57d808750ba55e745c4f9f030eda94b43af213
YasinMHussain <yasinmusfichussain@gmail.com>
2024-09-27 11:52:03 +0530

    Add files via upload

---
+++ srtlen.c
@@ -0,0 +1,14 @@
+//sting lentgh
+//Yasin M Hussain,27-09-2024
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char fname[20];
+	int x;
+	printf("\nEnter first name:");
+	scanf("%s",fname);
+	x=strlen(fname);
+	printf("\nString length:%d",x);
+}
