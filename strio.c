b63d032aa44dafa8ec989886615e7d75533b81cb
YasinMHussain <yasinmusfichussain@gmail.com>
2024-09-27 11:50:18 +0530

    Add files via upload

---
+++ strio.c
@@ -0,0 +1,12 @@
+//sting input output
+//Yasin M Hussain,27-09-2024
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char fname[20];
+	printf("\nEnter first name:");
+	scanf("%s",fname);
+	printf("\nFirst Name:%s",fname);
+}
