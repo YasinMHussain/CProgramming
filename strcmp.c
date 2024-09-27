c6fa51b5d998bf5fa26acf454e70f44fd898edf1
YasinMHussain <yasinmusfichussain@gmail.com>
2024-09-27 12:00:02 +0530

    Add files via upload

---
+++ strcmp.c
@@ -0,0 +1,19 @@
+//sting compare
+//Yasin M Hussain,27-09-2024
+
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char fname[20], sname[20];
+	int x;
+	printf("\nEnter first name:");
+	scanf("%s",fname);
+	printf("\nEnter second name:");
+	scanf("%s",sname);
+	x=strcmp(fname,sname);
+	if(x==1)
+		printf("\nThe strings are same");
+	else
+		printf("\nThe strings are not same");
+}
